7839042dc4f8cdb50479d421bb41139c8f6ac55d
syamers24 <ferdiansyah04022003@gmail.com>
2023-06-22 07:18:17 +0700

    v1.2

--- Js/script.js
+++ Js/script.js
@@ -1,3 +1,19 @@
+//function ctoc() {
+//let celcius = document.getElementById('cls')
 
-let celcius = document.getElementById('cls')
+//}
+function ctof(event) {
+    event.preventDefault();
 
+    var inputCelcius = document.getElementById("cels").value;
+    var fh = inputCelcius * (9 / 5) + 32;
+    document.getElementById("fahr").value = fh;
+    document.getElementById("how").value = inputCelcius + " * (9 / 5) + 32 = " +fh;
+}
+function reset(event) {
+    event.preventDefault();
+
+    document.getElementById("fahr").value = "0";
+    document.getElementById("cels").value = "0"; 
+    document.getElementById("how").value = inputCelcius + " * (9 / 5) + 32 = "+fh ;
+}
